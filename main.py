b4911f784f1c5dd416487027d8f97d6e77d0c35d
chloeiris <chloearoca@gmail.com>
2022-12-12 17:28:14 +0100

    main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import  streamlit_vertical_slider  as svs
+
 
 header = st.container()
 sliders = st.container()
@@ -21,4 +23,14 @@ with sliders:
     st.slider("100 is very popular", 0, 100)
     st.subheader('Energy')
     st.slider("100 is very energetic", 0, 100)
+    
+    svs.vertical_slider(key=3, 
+                    default_value=20, 
+                    step=10, 
+                    min_value=0, 
+                    max_value=100,
+                    slider_color= 'green' #optional
+                    track_color='lightgray' #optional
+                    thumb_color = 'red' #optional
+                    )
 
