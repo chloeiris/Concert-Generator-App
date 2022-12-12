fe6e87921dbacaf4ecc4e247550c18fe15e1cde2
chloeiris <chloearoca@gmail.com>
2022-12-12 17:00:01 +0100

    main

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+header = st.beta_container()
+plot = st.beta_container()
+setlist = st.beta_container()
+counters = st.beta_container()
+
+with header:
+    st.title('Setlist Generator')
+
+if __name__ == "__main__":
+    run()
