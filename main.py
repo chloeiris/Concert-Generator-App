49fd6cdb957796217d72145d94bd30b3fdd9d75c
Chloe <chloearoca@gmail.com>
2023-02-10 17:25:46 +0100

    checkboxes filter3

--- main.py
+++ main.py
@@ -33,26 +33,25 @@ with sliders:
     energy = st.slider("100 is very energetic", 0, 100)
     show_energy = st.checkbox("Apply energy filter")
 
+filters = []
+
 if show_energy:   
-    nrgy_filter = df['nrgy'].isin(range(energy-20, energy+20))
-else: nrgy_filter = ''
+    nrgy_filter = (df['nrgy'].isin(range(energy-20, energy+20)))
+    filters.append(nrgy_filter)
 
 if show_dnce:
-    dnce_filter = df['dnce'].isin(range(danceability-20, danceability+20))
-else:
-    dnce_filter = ''
+    dnce_filter = (df['dnce'].isin(range(danceability-20, danceability+20)))
+    filters.append(dnce_filter)
 
 if show_mood:
-    val_filter = df['val'].isin(range(mood-20, mood+20))
-else:
-    val_filter = ''
+    val_filter = (df['val'].isin(range(mood-20, mood+20)))
+    filters.append(val_filter)
 
 if show_pop:
-    pop_filter = df['pop'].isin(range(popularity-20, popularity+20))
-else:
-    pop_filter = ''
-
-df_filter = df[nrgy_filter & dnce_filter & val_filter & pop_filter]
+    pop_filter = (df['pop'].isin(range(popularity-20, popularity+20)))
+    filters.append(pop_filter)
+join_filters = '&'.join(filters)
+df_filter = df[join_filters]
 
 with setlist:
     show_all = st.checkbox("Show all songs in the database")
