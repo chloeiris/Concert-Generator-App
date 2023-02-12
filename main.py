e229bd01c57fc5e6a96bccb7df4bdcfafc30151d
Chloe <chloearoca@gmail.com>
2023-02-12 09:45:41 +0100

    plots1

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+#import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 
 df = pd.read_csv('topMusicJOB.csv', index_col = 0)
 
