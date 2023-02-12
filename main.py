520f70ebfe770a4ab923a97358c7ff746edcc93a
Chloe <chloearoca@gmail.com>
2023-02-12 12:04:11 +0100

    matplotllib requirements

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
-#import numpy as np
-#import matplotlib as plt
-#import seaborn as sns
+import matplotlib as plt
+
 
 df = pd.read_csv('topMusicJOB.csv', index_col = 0)
 
