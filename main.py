0628129f00ed1061187edae244b6528720af02ea
Chloe <chloearoca@gmail.com>
2023-02-22 10:13:38 +0100

    caching1

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
+@st.cache_data
+def load_data(path):
+    df = pd.read_csv(path, index_col = 0)
+    df.rename({'Unnamed: 0' : 'id', 'top genre': 'genre'}, axis=1, inplace=True)
+    return df
 
-df = pd.read_csv('topMusicJOB.csv', index_col = 0)
-
-df.rename({'Unnamed: 0' : 'id', 'top genre': 'genre'}, axis=1, inplace=True)
+df0 = load_data('topMusicJOB.csv')
 
 
 
 header = st.container()
-plot = st.container()
+sliders = st.sidebar()
 setlist = st.container()
 counters = st.container()
+plots = st.container()
 
 
 
@@ -23,47 +27,26 @@ with header:
 
 
     
-with st.sidebar:
+with sliders:
     st.title(':red[What kind of playlist are you going to create?]')
 
-    st.subheader('Mood')
-    mood = st.slider("100 is happy mood", 0, 100, step = 20)
-    show_mood = st.checkbox("Apply mood filter")
+    @st.cache_data
+    def gen_sliders(name, colname, message, start=0, stop=100, step=20):
+        st.subheader(name)
+        slider = st.slider(message, start, stop, step)
+        show_filter = st.checkbox(f"Apply {name.lower()} filter")
 
-    if show_mood:
-        val_filter = df['val'].isin(range(mood-20, mood+20))
-    else:
-        val_filter = df['val'].isin(range(100))
+        if show_filter:
+            filter = df0[colname].isin(range(slider-step, slider+step))
+        else:
+            filter = df0[colname].isin(range(stop))
 
+        return filter
 
-    st.subheader('Danceabilty')
-    danceability = st.slider("100 is very danceable", 0, 100, step = 20)
-    show_dnce = st.checkbox("Apply danceability filter")
-
-    if show_dnce:
-        dnce_filter = df['dnce'].isin(range(danceability-20, danceability+20))
-    else:
-        dnce_filter = df['dnce'].isin(range(100))
-            
-
-    st.subheader('Popularity')
-    popularity = st.slider("100 is very popular", 0, 100, step = 20)
-    show_pop = st.checkbox("Apply popularity filter")
-
-    if show_pop:
-        pop_filter = df['pop'].isin(range(popularity-20, popularity+20))
-    else:
-        pop_filter = df['pop'].isin(range(100))
-    
-
-    st.subheader('Energy')
-    energy = st.slider("100 is very energetic", 0, 100, step = 20)
-    show_energy = st.checkbox("Apply energy filter")
-
-    if show_energy:   
-        nrgy_filter = df['nrgy'].isin(range(energy-20, energy+20))
-    else:
-        nrgy_filter = df['nrgy'].isin(range(100))
+    mood_filter = gen_sliders('Mood', 'val', '100 is happy mood')
+    dnce_filter = gen_sliders('Danceabilty', 'dnce', '100 is very danceable')
+    pop_filter = gen_sliders('Popularity', 'pop', '100 is very popular')
+    nrgy_filter = gen_sliders('Energy', 'nrgy', '100 is very energetic')
 
 
 
@@ -71,15 +54,12 @@ with setlist:
     show_all = st.checkbox("Show all songs in the database")
 
     if show_all:
-        st.dataframe(df)
+        data = st.dataframe(df0)
+        data2 = pd.DataFrame(data)
     else:
-        #try:
-        df_filter = df[val_filter & dnce_filter & pop_filter & nrgy_filter]
-        st.dataframe(df_filter)
-
-        #except:
-            #st.write("No filters selected.")
-        
+        df_filter = df0[mood_filter & dnce_filter & pop_filter & nrgy_filter]
+        data = st.dataframe(df_filter)
+        data2 = pd.DataFrame(data)
 
 
 
@@ -87,125 +67,95 @@ with counters:
     st.header("Counters")
 
     st.subheader("Total Duration")
+    def counter(dataf):
+        hours = int(dataf['dur'].sum() / 3600)
+        mod_hour = dataf['dur'].sum() % 3600
+        minutes = int((mod_hour) / 60)
+        seconds = mod_hour % 60
+        return st.write(hours, 'hours', ',', minutes, 'minutes', ',', seconds, 'seconds')
+
     if show_all:
-        hours = int(df['dur'].sum() / 3600)
-        mod_hour = df['dur'].sum() % 3600
-        minutes = int((mod_hour) / 60)
-        seconds = mod_hour % 60
-        st.write(hours, 'hours', ',', minutes, 'minutes', ',', seconds, 'seconds')
+        counter(df0)
     else:
-        #try:
-        hours = int(df_filter['dur'].sum() / 3600)
-        mod_hour = df_filter['dur'].sum() % 3600
-        minutes = int((mod_hour) / 60)
-        seconds = mod_hour % 60
-        st.write(hours, 'hours', ',', minutes, 'minutes', ',', seconds, 'seconds')
-        #except:
-            #st.write("No filters selected.")
+        counter(df_filter)
 
 
     st.subheader("Number of Songs")
     if show_all:
-        st.write(df.shape[0])
+        st.write(df0.shape[0])
     else:
-        #try:
         st.write(df_filter.shape[0])
-        #except:
-            #st.write("No filters selected.")
+        
 
 
+with plots:
+    st.header("Playlist Visualizaton")
 
-st.header("Playlist Traits Viz")
+    tab1, tab2 = st.tabs(["Main traits", "Playlist Flow"])
 
-tab1, tab2 = st.tabs(["Main traits", "Playlist Flow"])
+    @st.cache_data
+    def check_char(name):
+        check_box = tab2.checkbox(f"Show {name.lower()}")
+        return check_box
+    
+    check_mood = check_char("mood")
+    check_dnce = check_char("danceability")
+    check_pop = check_char("popularity")
+    check_nrgy = check_char("energy")
+    
+    def boxplots_chars(dataf, colnames, labels, facecolors, colors):
+        fig, axes = plt.subplots(figsize=(20, 15))
+        axes.boxplot(x=dataf[colnames[0]], widths=0.25, positions=[0.5], patch_artist= True, notch=False, labels=[labels[0]],
+                        boxprops=dict(facecolor=facecolors[0], color=colors[0], linewidth=1.5),
+                        capprops=dict(color=colors[0], linewidth=1.5),
+                        whiskerprops=dict(color=colors[0], linewidth=1.5),
+                        flierprops=dict(color=colors[0], markeredgecolor=colors[0], linewidth=1.5),
+                        medianprops=dict(color=colors[0], linewidth=1.5))
+        axes.boxplot(x=dataf[colnames[1]], widths=0.25, positions=[0.5], patch_artist= True, notch=False, labels=[labels[1]],
+                        boxprops=dict(facecolor=facecolors[1], color=colors[1], linewidth=1.5),
+                        capprops=dict(color=colors[1], linewidth=1.5),
+                        whiskerprops=dict(color=colors[1], linewidth=1.5),
+                        flierprops=dict(color=colors[1], markeredgecolor=colors[1], linewidth=1.5),
+                        medianprops=dict(color=colors[1], linewidth=1.5))
+        axes.boxplot(x=dataf[colnames[2]], widths=0.25, positions=[0.5], patch_artist= True, notch=False, labels=[labels[2]],
+                        boxprops=dict(facecolor=facecolors[2], color=colors[2], linewidth=1.5),
+                        capprops=dict(color=colors[2], linewidth=1.5),
+                        whiskerprops=dict(color=colors[2], linewidth=1.5),
+                        flierprops=dict(color=colors[2], markeredgecolor=colors[2], linewidth=1.5),
+                        medianprops=dict(color=colors[2], linewidth=1.5))
+        axes.boxplot(x=dataf[colnames[3]], widths=0.25, positions=[0.5], patch_artist= True, notch=False, labels=[labels[3]],
+                        boxprops=dict(facecolor=facecolors[3], color=colors[3], linewidth=1.5),
+                        capprops=dict(color=colors[3], linewidth=1.5),
+                        whiskerprops=dict(color=colors[3], linewidth=1.5),
+                        flierprops=dict(color=colors[3], markeredgecolor=colors[3], linewidth=1.5),
+                        medianprops=dict(color=colors[3], linewidth=1.5))
+        return tab1.pyplot(fig)
+    
 
-check_mood = tab2.checkbox("Show mood")
-check_dnce = tab2.checkbox("Show danceability")
-check_pop = tab2.checkbox("Show popularity")
-check_nrgy = tab2.checkbox("Show energy")
-
-if show_all:
-
-    fig, axes = plt.subplots(figsize=(20, 15))
-    axes.boxplot(x=df['val'], widths=0.25, positions=[0.5], patch_artist= True, notch=False, labels=["mood"],
-                    boxprops=dict(facecolor='mediumvioletred', color='red', linewidth=1.5),
-                    capprops=dict(color="red", linewidth=1.5),
-                    whiskerprops=dict(color="red", linewidth=1.5),
-                    flierprops=dict(color="red", markeredgecolor="red", linewidth=1.5),
-                    medianprops=dict(color="red", linewidth=1.5))
-    axes.boxplot(x=df['dnce'], widths=0.25, positions=[1], patch_artist= True, notch=False, labels=["danceability"], 
-                    boxprops=dict(facecolor='lightseagreen', color='darkorange', linewidth=1.5),
-                    capprops=dict(color="darkorange", linewidth=1.5),
-                    whiskerprops=dict(color="darkorange", linewidth=1.5),
-                    flierprops=dict(color="darkorange", markeredgecolor="darkorange", linewidth=1.5),
-                    medianprops=dict(color="darkorange", linewidth=1.5))
-    axes.boxplot(x=df['nrgy'], widths=0.25, positions=[1.5], patch_artist= True, notch=False, labels=["popularity"], 
-                    boxprops=dict(facecolor='mediumseagreen', color='orange', linewidth=1.5),
-                    capprops=dict(color="orange", linewidth=1.5),
-                    whiskerprops=dict(color="orange", linewidth=1.5),
-                    flierprops=dict(color="orange", markeredgecolor="orange", linewidth=1.5),
-                    medianprops=dict(color="orange", linewidth=1.5))
-    axes.boxplot(x=df['nrgy'], widths=0.25, positions=[2], patch_artist= True, notch=False, labels=["energy"], 
-                    boxprops=dict(facecolor='orangered', color='lawngreen', linewidth=1.5),
-                    capprops=dict(color="lawngreen", linewidth=1.5),
-                    whiskerprops=dict(color="lawngreen", linewidth=1.5),
-                    flierprops=dict(color="lawngreen", markeredgecolor="lawngreen", linewidth=1.5),
-                    medianprops=dict(color="lawngreen", linewidth=1.5))
-    tab1.pyplot(fig)
-
-    fig2, axes2 = plt.subplots(figsize=(20, 15))
-    if check_mood:
-        sns.lineplot(x=np.linspace(0,df['dur'].sum(),num=df.shape[0]), y=df['val'], data=df, ax=axes2, color='mediumvioletred', label='mood')
-    if check_dnce:
-        sns.lineplot(x=np.linspace(0,df['dur'].sum(),num=df.shape[0]), y=df['dnce'], data=df, ax=axes2, color='lightseagreen', label='danceability')
-    if check_pop:
-        sns.lineplot(x=np.linspace(0,df['dur'].sum(),num=df.shape[0]), y=df['pop'], data=df, ax=axes2, color='mediumseagreen', label='popularity')
-    if check_nrgy:
-        sns.lineplot(x=np.linspace(0,df['dur'].sum(),num=df.shape[0]), y=df['nrgy'], data=df, ax=axes2, color='orangered', label='energy')
-    axes2.legend(fontsize=15)
-    tab2.pyplot(fig2)
-
-else:
-    fig, axes = plt.subplots(figsize=(20, 15))
-    axes.boxplot(x=df_filter['val'], widths=0.25, positions=[0.5], patch_artist= True, notch=False, labels=["mood"],
-                    boxprops=dict(facecolor='mediumvioletred', color='red', linewidth=1.5),
-                    capprops=dict(color="red", linewidth=1.5),
-                    whiskerprops=dict(color="red", linewidth=1.5),
-                    flierprops=dict(color="red", markeredgecolor="red", linewidth=1.5),
-                    medianprops=dict(color="red", linewidth=1.5))
-    axes.boxplot(x=df_filter['dnce'], widths=0.25, positions=[1], patch_artist= True, notch=False, labels=["danceability"], 
-                    boxprops=dict(facecolor='lightseagreen', color='darkorange', linewidth=1.5),
-                    capprops=dict(color="darkorange", linewidth=1.5),
-                    whiskerprops=dict(color="darkorange", linewidth=1.5),
-                    flierprops=dict(color="darkorange", markeredgecolor="darkorange", linewidth=1.5),
-                    medianprops=dict(color="darkorange", linewidth=1.5))
-    axes.boxplot(x=df_filter['nrgy'], widths=0.25, positions=[1.5], patch_artist= True, notch=False, labels=["popularity"], 
-                    boxprops=dict(facecolor='mediumseagreen', color='orange', linewidth=1.5),
-                    capprops=dict(color="orange", linewidth=1.5),
-                    whiskerprops=dict(color="orange", linewidth=1.5),
-                    flierprops=dict(color="orange", markeredgecolor="orange", linewidth=1.5),
-                    medianprops=dict(color="orange", linewidth=1.5))
-    axes.boxplot(x=df_filter['nrgy'], widths=0.25, positions=[2], patch_artist= True, notch=False, labels=["energy"], 
-                    boxprops=dict(facecolor='orangered', color='lawngreen', linewidth=1.5),
-                    capprops=dict(color="lawngreen", linewidth=1.5),
-                    whiskerprops=dict(color="lawngreen", linewidth=1.5),
-                    flierprops=dict(color="lawngreen", markeredgecolor="lawngreen", linewidth=1.5),
-                    medianprops=dict(color="lawngreen", linewidth=1.5))
-    tab1.pyplot(fig)
-
-
-    fig2, axes2 = plt.subplots(figsize=(20, 15))
-    if check_mood:
-        sns.lineplot(x=np.linspace(0,df_filter['dur'].sum(),num=df_filter.shape[0]), y=df_filter['val'], data=df_filter, ax=axes2, color='mediumvioletred', label='mood')
-    if check_dnce:
-        sns.lineplot(x=np.linspace(0,df_filter['dur'].sum(),num=df_filter.shape[0]), y=df_filter['dnce'], data=df_filter, ax=axes2, color='lightseagreen', label='danceability')
-    if check_pop:
-        sns.lineplot(x=np.linspace(0,df_filter['dur'].sum(),num=df_filter.shape[0]), y=df_filter['pop'], data=df_filter, ax=axes2, color='mediumseagreen', label='popularity')
-    if check_nrgy:
-        sns.lineplot(x=np.linspace(0,df_filter['dur'].sum(),num=df_filter.shape[0]), y=df_filter['nrgy'], data=df_filter, ax=axes2, color='orangered', label='energy')
-    axes2.legend(fontsize=15)
-    tab2.pyplot(fig2)
-
-
+    def line_plots(dfr, colnames, colors, labels):
+        fig2, axes2 = plt.subplots(figsize=(20, 15))
+        if check_mood:
+            sns.lineplot(x=np.linspace(0,dfr['dur'].sum(),num=dfr.shape[0]), y=dfr[colnames[0]], data=dfr, ax=axes2, color=colors[0], label=labels[0])
+        if check_dnce:
+            sns.lineplot(x=np.linspace(0,dfr['dur'].sum(),num=dfr.shape[0]), y=dfr[colnames[1]], data=dfr, ax=axes2, color=colors[1], label=labels[1])
+        if check_pop:
+            sns.lineplot(x=np.linspace(0,dfr['dur'].sum(),num=dfr.shape[0]), y=dfr[colnames[2]], data=dfr, ax=axes2, color=colors[2], label=labels[2])
+        if check_nrgy:
+            sns.lineplot(x=np.linspace(0,dfr['dur'].sum(),num=dfr.shape[0]), y=dfr[colnames[3]], data=dfr, ax=axes2, color=colors[3], label=labels[3])
+        axes2.legend(fontsize=15)
+        return tab2.pyplot(fig2)
 
     
+    colnames = ['val', 'dnce', 'pop', 'nrgy']
+    labels = ['mood', 'danceability', 'popularity', 'energy']
+    facecolors = ['mediumvioletred', 'lightseagreen', 'mediumseagreen', 'lawngreen']
+    colors = ['red', 'darkorange', 'orange', 'lawngreen']
+
+    boxplots_chars(data2, colnames, labels, facecolors, colors)
+    line_plots(data2, colnames, colors, labels)
+
+    
+
+
+
+        
